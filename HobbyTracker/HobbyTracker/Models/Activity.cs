1afc18a6dc2f7bf4a8887958ad103332ed6f7873
Zhaoqi Liu <zhaoqi0904@hotmail.com>
2015-04-23 03:04:58 +0000

    Activity column name and index page changing

--- HobbyTracker/HobbyTracker/Models/Activity.cs
+++ HobbyTracker/HobbyTracker/Models/Activity.cs
@@ -1,23 +1,29 @@
 ﻿using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace HobbyTracker.Models
 {
     public class Activity
     {
         [Key]
+
         public int ActivityID { get; set; }
+        [DisplayName("Activity Name")]
         [Required(ErrorMessage = "Please enter your activity name")]
         public string ActName { get; set; }
         [Required(ErrorMessage = "Please enter your email address")]
         [RegularExpression(".+\\@.+\\..+",
             ErrorMessage = "Please enter a valid email address")]
+        [DisplayName("Email Address")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Please enter your phone number")]
+        [DisplayName("Phone Number")]
         public string Phone { get; set; }
+        [DisplayName("Will you attend?")]
         [Required(ErrorMessage = "Please specify wheter you'll attend")]
         public bool? WillAttend { get; set; }
-
+        [DisplayName("Community Name")]
         public int CommunityID { get; set; }
 
 
